fc514ff9a5eff6308fbce710a2fb7ceb2e52e74d
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-09-05 17:17:45 +0300

    update function validForm()

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const startPage = document.getElementsByClassName("startPage");
+const startPage = document.getElementById("startPage");
 const btnSignIn = document.getElementById("signIn");
 const registrationModal = document.getElementById("registerModal");
 const inputUserName = document.getElementById("inputUserName");
@@ -11,24 +11,42 @@ const inputCheck = document.getElementById("inputCheck");
 const btnCreateAccount = document.getElementById("btnCreateAccount");
 const welcomeWindow = document.getElementById("welcomeModal");
 
-btnSignIn.addEventListener("click", function openModalWindow() {
-  startPage[0].style.display = "none";
+let errorUserName = document.getElementById("errorUserName");
+let errorEmail = document.getElementById("errorEmail");
+let errorPassword = document.getElementById("errorPassword");
+let errorRepeatPassword = document.getElementById("errorRepeatPassword");
+let errorPhone = document.getElementById("errorPhone");
+let errorInputChecked = document.getElementById("errorInputChecked");
+const errors = [
+  errorUserName,
+  errorEmail,
+  errorPassword,
+  errorRepeatPassword,
+  errorPhone,
+  errorInputChecked,
+];
+
+btnSignIn.addEventListener("click", function () {
+  startPage.style.display = "none";
   registrationModal.style.display = "block";
 });
 
 function validForm() {
-  document.getElementById("errorUserName").innerHTML = "";
-  document.getElementById("errorEmail").innerHTML = "";
-  document.getElementById("errorPassword").innerHTML = "";
-  document.getElementById("errorRepeatPassword").innerHTML = "";
-  document.getElementById("errorPhone").innerHTML = "";
-  document.getElementById("errorInputChecked").innerHTML = "";
+  errors.forEach((elem) => {
+    elem.innerHTML = "";
+  });
+  // errorUserName.innerHTML = "";
+  // errorEmail.innerHTML = "";
+  // errorPassword.innerHTML = "";
+  // errorRepeatPassword.innerHTML = "";
+  // errorPhone.innerHTML = "";
+  // errorInputChecked.innerHTML = "";
 
   let isValid = true;
 
   if (inputUserName.value == "" || inputUserSurname.value == "") {
     isValid = false;
-    document.getElementById("errorUserName").innerHTML +=
+    errorUserName.innerHTML +=
       "You have not filled in the first name and/or surname field<br/>";
   }
 
@@ -37,57 +55,52 @@ function validForm() {
     (inputUserSurname.value.length > 0 && inputUserSurname.value.length < 2)
   ) {
     isValid = false;
-    document.getElementById("errorUserName").innerHTML +=
-      "Your name and/or surname is too short";
+    errorUserName.innerHTML += "Your name and/or surname is too short";
   }
 
   if (inputEmail.value == "") {
     isValid = false;
-    document.getElementById("errorEmail").innerHTML +=
-      "You did not provide an email";
+    errorEmail.innerHTML += "You did not provide an email";
   }
 
   if (inputPassword.value == "") {
     isValid = false;
-    document.getElementById("errorPassword").innerHTML +=
-      "You need to think of a password";
+    errorPassword.innerHTML += "You need to think of a password";
   }
 
   if (inputPassword.value.length > 0 && inputPassword.value.length < 6) {
     isValid = false;
-    document.getElementById("errorPassword").innerHTML +=
+    errorPassword.innerHTML +=
       "Your password is too short, a minimum of 6 characters is allowed";
     console.log(345);
   }
 
   if (inputPassword.value != inputRepeatPassword.value) {
     isValid = false;
-    document.getElementById("errorRepeatPassword").innerHTML +=
-      "Passwords do not match";
+    errorRepeatPassword.innerHTML += "Passwords do not match";
   }
 
   if (inputPhone.value == "") {
     isValid = false;
-    document.getElementById("errorPhone").innerHTML +=
-      "You did not provide a mobile phone number";
+    errorPhone.innerHTML += "You did not provide a mobile phone number";
   }
 
   if (inputPhone.value.length > 0 && inputPhone.value.length < 12) {
     isValid = false;
-    document.getElementById("errorPhone").innerHTML +=
+    errorPhone.innerHTML +=
       "Are you sure you have the right phone number? Perhaps you have missed a few digits.";
   }
 
   if (!inputCheck.checked) {
     isValid = false;
-    document.getElementById("errorInputChecked").innerHTML +=
+    errorInputChecked.innerHTML +=
       "You need to tick the box if you agree with Policy.";
   }
 
   return isValid;
 }
 
-btnCreateAccount.addEventListener("click", function singIn() {
+btnCreateAccount.addEventListener("click", function () {
   if (validForm()) {
     registrationModal.style.display = "none";
     welcomeWindow.style.display = "block";
