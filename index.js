f8283e3b1bdfb4da464d77e5d738942bd8fee80b
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-09-19 17:40:46 +0300

    update validation

--- index.js
+++ index.js
@@ -35,44 +35,52 @@ function validForm() {
   errors.forEach((elem) => {
     elem.innerHTML = "";
   });
-  // errorUserName.innerHTML = "";
-  // errorEmail.innerHTML = "";
-  // errorPassword.innerHTML = "";
-  // errorRepeatPassword.innerHTML = "";
-  // errorPhone.innerHTML = "";
-  // errorInputChecked.innerHTML = "";
 
   let isValid = true;
 
+  let userNameFormat = /^[a-zA-Z'][a-zA-Z-' ]+[a-zA-Z']?$/;
   if (inputUserName.value == "" || inputUserSurname.value == "") {
     isValid = false;
     errorUserName.innerHTML +=
-      "You have not filled in the first name and/or surname field<br/>";
-  }
-
-  if (
+      "You have not filled in the first name and/or surname field <br>";
+  } else if (
     (inputUserName.value.length > 0 && inputUserName.value.length < 2) ||
     (inputUserSurname.value.length > 0 && inputUserSurname.value.length < 2)
   ) {
     isValid = false;
     errorUserName.innerHTML += "Your name and/or surname is too short";
+  } else if (
+    !inputUserName.value.match(userNameFormat) ||
+    !inputUserSurname.value.match(userNameFormat)
+  ) {
+    isValid = false;
+    errorUserName.innerHTML += "You can only use Latin letters <br>";
   }
 
+  let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
   if (inputEmail.value == "") {
     isValid = false;
-    errorEmail.innerHTML += "You did not provide an email";
+    errorEmail.innerHTML += "You did not provide an email <br>";
+  } else if (!inputEmail.value.match(mailFormat)) {
+    isValid = false;
+    errorEmail.innerHTML +=
+      "Email may only contain Latin letters and numbers (e.g. example@mail.com)";
   }
 
+  let passwordFormat =
+    /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
   if (inputPassword.value == "") {
     isValid = false;
-    errorPassword.innerHTML += "You need to think of a password";
-  }
-
-  if (inputPassword.value.length > 0 && inputPassword.value.length < 6) {
+    errorPassword.innerHTML +=
+      "You need to think of a password using Latin letters, numbers and symbols (!, @, #, $, &). Length of password may be of at least 6 characters";
+  } else if (inputPassword.value.length > 0 && inputPassword.value.length < 6) {
     isValid = false;
     errorPassword.innerHTML +=
       "Your password is too short, a minimum of 6 characters is allowed";
-    console.log(345);
+  } else if (!inputPassword.value.match(passwordFormat)) {
+    isValid = false;
+    errorPassword.innerHTML +=
+      'Password may only contain Latin letters, numbers and symbols "!, @, #, $, &". Length of password may be of at least 6 characters';
   }
 
   if (inputPassword.value != inputRepeatPassword.value) {
@@ -80,12 +88,14 @@ function validForm() {
     errorRepeatPassword.innerHTML += "Passwords do not match";
   }
 
+  let phoneFormat = /(?:\+|\d)[\d\-\(\)]\d/g;
   if (inputPhone.value == "") {
     isValid = false;
     errorPhone.innerHTML += "You did not provide a mobile phone number";
-  }
-
-  if (inputPhone.value.length > 0 && inputPhone.value.length < 12) {
+  } else if (!inputPhone.value.match(phoneFormat)) {
+    isValid = false;
+    errorPhone.innerHTML += "You may use only numbers";
+  } else if (inputPhone.value.length > 0 && inputPhone.value.length < 12) {
     isValid = false;
     errorPhone.innerHTML +=
       "Are you sure you have the right phone number? Perhaps you have missed a few digits.";
